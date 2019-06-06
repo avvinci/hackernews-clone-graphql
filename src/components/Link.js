72b32ebda85a86bbef405fc725110bb648c8dae1
avvinci <vinciabhinav7@gmail.com>
2019-06-06 16:07:19 +0530

    added vote, postby, date to link component

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,11 +1,30 @@
 import React , { Component } from 'react'
+import { AUTH_TOKEN } from '../constant';
+import { timeDifferenceForDate } from '../utils';
 
 class Link extends Component {
     render(){
+        const auth_token = localStorage.getItem(AUTH_TOKEN)
         return (
-            <div>
-                <div>
-                    {this.props.link.description} ({this.props.link.url})
+            <div className = "flex mt2 item-start">
+                <div className = "flex item-center">
+                    <span className ="gray">  {this.props.index + 1}.</span>
+                    { auth_token && (
+                        <div className ="ml1 gray f11"
+                            onClick = { () => this._voteForLink() }
+                        >▲</div>
+                    )}
+                </div>
+                <div className ="ml1">
+                    <div>
+                        {this.props.link.description} ({this.props.link.url})
+                    </div>
+                    <div className ="f6 lh-copy gray">
+                        {this.props.link.votes.length} votes | {' '}
+                        {this.props.postedBy ? this.props.postedBy.name : 'Unknown'}
+                        {' '}
+                        {timeDifferenceForDate(this.props.link.createdAt)}
+                    </div>
                 </div>
             </div>
         )
