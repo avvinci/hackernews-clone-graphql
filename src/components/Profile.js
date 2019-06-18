f6cc177ab8ac4bf08be400f0e7ed9663f83c485c
avvinci <vinciabhinav7@gmail.com>
2019-06-18 12:02:38 +0530

    Posts by user frontend done

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,7 @@
 import React, { Component , Fragment } from 'react'
 import { Query } from 'react-apollo'
 import { AUTH_TOKEN } from '../constant';
+import Link  from "./Link"
 import { timeDifferenceForDate } from '../utils';
 import gql from "graphql-tag";
 
@@ -13,38 +14,84 @@ export const USER_QUERY = gql`
     }
   }
 `
+export const LINKS_BY_USER = gql`
+  query LinksByUser{
+      linksByUser{
+        id
+        createdAt
+        url
+        description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+    }
+  }
+`
 
 class Profile extends Component {
     render(){
         const auth_token = localStorage.getItem(AUTH_TOKEN)
         return(
-            <Query query = { USER_QUERY }  >
-              {( {loading, error, data  }) => {
-                if(loading) return <div>Fetching</div>
-                if(error) return <div> Error</div>
-                return(
-                  <Fragment>
-                    <div className = "flex mt2 items-start">
+            <Fragment>
 
-                        <div className = "flex items-center">
-                            { auth_token && ( <div>
-                                Welcome !! 
-                                {data.user.name}{'. email - '}
-                                {data.user.email}
+
+                
+                <Query query = { USER_QUERY }  >
+                {( {loading, error, data  }) => {
+                    if(loading) return <div>Fetching</div>
+                    if(error) return <div> Error</div>
+                    return(
+                    <Fragment>
+                        <div className = "flex mt2 items-start">
+
+                            <div className = "flex items-center">
+                                { auth_token && ( <div>
+                                    Welcome !! 
+                                    {data.user.name}{'. email - '}
+                                    {data.user.email}
+                                    </div>
+                                ) }
+            
+
+                            </div>
+
+                            <div className ="ml1">
+                            </div>
+
+                        </div>
+                    </Fragment>
+                    )
+                }}
+                </Query>
+                <br />
+                <p >Posts by user</p> 
+
+                <Query query = {LINKS_BY_USER} >
+                    {({loading,error,data}) => {
+                            if(loading) return <div>Fetching</div>
+                            if(error) return <div> Error</div>
+                            return(
+                                <div>
+                                    {data.linksByUser.map( (link) => (
+                                    < Link 
+                                        key ={link.id} 
+                                        link = {link} 
+                                        />
+                                        ))}
                                 </div>
-                            ) }
-        
+                            )
+                   }}
 
-                        </div>
+                </Query>
 
-                        <div className ="ml1">
-                        </div>
-
-                    </div>
-                  </Fragment>
-                )
-              }}
-            </Query>
+            </Fragment>
         )
     }
 }
