1db46662b814ed5e9c12579f36a8689001f5a477
avvinci <vinciabhinav7@gmail.com>
2019-06-18 09:52:58 +0530

    user query done for profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,27 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component , Fragment } from 'react'
+import { Query } from 'react-apollo'
 import { AUTH_TOKEN } from '../constant';
 import { timeDifferenceForDate } from '../utils';
-import {Mutation} from 'react-apollo'
 import gql from "graphql-tag";
 
 
+export const USER_QUERY = gql`
+  query UserQuery {
+    user {
+      name
+      email
+    }
+  }
+`
 
 class Profile extends Component {
     render(){
         const auth_token = localStorage.getItem(AUTH_TOKEN)
-        return (
-            <div className = "flex mt2 items-start">
+        return(
+            <Query query = { USER_QUERY }  >
+              {( {loading, error, data  }) => {
+                if(loading) return <div>Fetching</div>
+                if(error) return <div> Error</div>
+                return(
+                  <Fragment>
+                    <div className = "flex mt2 items-start">
 
-                <div className = "flex items-center">
-                    { auth_token && (
-                        'Welcome User'
-                    )}
-                </div>
+                        <div className = "flex items-center">
+                            { auth_token && ( <div>
+                                Welcome !! 
+                                {data.user.name}{'. email - '}
+                                {data.user.email}
+                                </div>
+                            ) }
+        
 
-                <div className ="ml1">
-                </div>
+                        </div>
 
-            </div>
+                        <div className ="ml1">
+                        </div>
+
+                    </div>
+                  </Fragment>
+                )
+              }}
+            </Query>
         )
     }
 }
